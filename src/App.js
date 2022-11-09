597dff1e51006469e421beecab764fd36410b50d
CaptainOfTheSky <aetyler14@gmail.com>
2022-11-09 10:36:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef } from 'react';
 
 function App() {
+  const val = useRef(null);
+  const [names, setNames] = useState(['John', 'Katie']);
+
+  const addName = () => {
+    console.log(val.current.value);
+
+    if(val.current.value != null && val !=='') {
+      setNames(prev => [...prev, val.current.value]);
+      // val.current.value = '';
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" ref={val} />
+      <button onClick={addName}>Add</button>
+
+      <div>
+        {names && names.map((cust, index) =>
+          <div key={index} className="card">{cust}</div>
+        )}
+
+      </div>
     </div>
   );
 }
